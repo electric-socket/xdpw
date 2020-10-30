2804b8fec1f67201ce1447b22209914850547331
electric-socket <72189474+electric-socket@users.noreply.github.com>
2020-10-30 19:50:58 -0400

    Version 0.14.0
    New version 0.14.0 with several changes:
    Compiler now accepts the older (* *) style comments in addition to { } and single-line // comments. If you fail to close a comment the compiler not only reports this at the end of file, but the line and position where the last comment was open. If you accidentally use a } without an open brace the compiler says so.
    Added local time function to Sysutils, allowing the compiler to report when the compiler was started and when it ended
    Many new statistics reported: Compiler prints number of lines for each file and total number of lines compiled. It also reports compilation speed in lines per second, which on my several year old computer was more than 48,000 lines/second. The compiler can compile itself - a 12,000 line program in about six modules - in 0.25 seconds. This release also includes a pre-compiled compiler so you don't have to have Delphi or Free Pascal to use it.
    Code sizes are reported in decimal and hexadecimal.

--- Scanner.pas
+++ Scanner.pas
@@ -1,6 +1,8 @@
 // XD Pascal - a 32-bit compiler for Windows
 // Copyright (c) 2009-2010, 2019-2020, Vasiliy Tereshkov
 
+// VERSION 0.14;0
+
 {$I-}
 {$H-}
 
@@ -10,7 +12,7 @@ unit Scanner;
 interface
 
 
-uses Common;
+uses Common,SysUtils;
 
 
 var
@@ -44,7 +46,10 @@ type
 
   TScannerState = record
     Token: TToken;
+    CommentFile,
     FileName: TString;
+    CommentPoint,
+    CommentLine,
     Position,
     Line: Integer;
     Buffer: TBuffer;
@@ -99,6 +104,8 @@ with ScannerState do
   FileName := Name;
   Line := 1;
   Position := 0;
+  CommentLine := 0;
+  CommentPoint := 0;
   
   with Buffer do
     begin
@@ -229,17 +236,50 @@ end;
 
 
 
-procedure ReadMultiLineComment;
+procedure ReadMultiLineComment(Oldschool: Boolean);
+
+    Procedure CommentEof; // eof in middle of a comment
+    begin // provide an alternative error message for "runaway" comment
+       with ScannerState do
+          begin
+            Notice(ScannerFileName + ' (' + IntToStr(ScannerLine) +
+                  ':' + IntToStr(ScannerPos)  + ') Error: End of file inside comment');
+            Notice('Attention: Your last comment began at ('+
+                              IntToStr(CommentLine)+':'+IntToStr(CommentPoint)+
+                              ') and it might be helpful to look there.');
+            repeat FinalizeScanner until not RestoreScanner;
+            FinalizeCommon;
+            Halt(1);
+          end
+     end;
+
 begin
-with ScannerState do
-  while (ch <> '}') and not EndOfUnit do
-    ReadChar(ch);
+    with ScannerState do
+        if not oldschool then
+        begin
+           while (ch <> '}') and not EndOfUnit do
+              ReadChar(ch);
+           if EndofUnit then
+              CommentEof;
+         end
+         else     // older comment starting with (* must end with *)
+         repeat
+                readchar(ch);
+                if endofunit then CommentEof;
+                if ch='*' then // check for close of comment
+                 begin
+                   readchar(ch);
+                   if endofunit then CommentEof;
+                   if ch=')' then
+                        exit;        // it is closed
+                end
+           until EndOfUnit;
 end;
 
 
 
 
-procedure ReadDirective;
+procedure ReadDirective(OldSchool:boolean);
 var
   Text: TString;
 begin
@@ -288,13 +328,13 @@ with ScannerState do
     end    
     
   else                                  // All other directives are ignored
-    ReadMultiLineComment;
+    ReadMultiLineComment(OldSchool);
   end;  
 end;
 
 
 
-
+// hexadecimal numbers
 procedure ReadHexadecimalNumber;
 var
   Num, Digit: Integer;
@@ -330,7 +370,7 @@ end;
 
 
 
-
+// integers and floating-point numbers
 procedure ReadDecimalNumber;
 var
   Num, Expon, Digit: Integer;
@@ -433,7 +473,7 @@ end;
 
 
 
-
+// Determine type of number
 procedure ReadNumber;
 begin
 with ScannerState do
@@ -448,7 +488,7 @@ end;
 
 
 
-
+// red #nnn or #$nn char
 procedure ReadCharCode;
 begin
 with ScannerState do
@@ -469,7 +509,7 @@ end;
 
 
 
-
+// Is it a keyword or an identifier? Read more and find out!!
 procedure ReadKeywordOrIdentifier;
 var
   Text, NonUppercaseText: TString;
@@ -501,7 +541,7 @@ end;
 
 
 
-
+ // get one character or a string
 procedure ReadCharOrStringLiteral;
 var
   Text: TString;
@@ -548,7 +588,7 @@ end;
 
 
 
-
+// read the next noken in sequence
 procedure NextTok;
 begin
 with ScannerState do
@@ -556,24 +596,44 @@ with ScannerState do
   Token.Kind := EMPTYTOK;
 
   // Skip spaces, comments, directives
-  while (ch in Spaces) or (ch = '{') or (ch = '/') do
+  while (ch in Spaces) or (ch = '{') or
+        (ch = '/') or (ch='(')  do
     begin
-    if ch = '{' then                                                      // Multi-line comment or directive
+    if ch = '{' then   // handle mewer { comments }                                                   // Multi-line comment or directive
       begin
-      ReadUppercaseChar(ch);
-      if ch = '$' then ReadDirective else ReadMultiLineComment;
+      Commentpoint := Position ;  // for "runaway" comments
+      CommentLine := Line;
+      ReadChar(ch);
+      if ch = '$' then ReadDirective(FALSE) else ReadMultiLineComment(FALSE);
       end
     else if ch = '/' then
       begin
-      ReadUppercaseChar(ch2);
+      ReadChar(ch2);
       if ch2 = '/' then
         ReadSingleLineComment                                             // Double-line comment
       else
         begin
-        if not EndOfUnit then Dec(Buffer.Pos);                            // Discard ch2     
+        if not EndOfUnit then Dec(Buffer.Pos);  // put the character back                          // Discard ch2
+        Break;
+        end;
+      end
+    else if ch = '(' then // handle old-school (* comments *)
+      begin
+          ReadChar(ch2);
+          if ch2 = '*' then
+          begin
+              Commentpoint := Position -1;  // for "runaway" comments
+              CommentLine := Line;
+              ReadChar(ch);
+              if ch = '$' then ReadDirective(TRUE) else ReadMultiLineComment(TRUE);
+          end
+      else
+        begin
+        if not EndOfUnit then Dec(Buffer.Pos);  // return borrowed char                          // Discard ch2
         Break;
         end;
       end;
+
     ReadChar(ch);
     end;
 
@@ -646,9 +706,10 @@ with ScannerState do
       '^': Token.Kind := DEREFERENCETOK;
       '@': Token.Kind := ADDRESSTOK;
       '[': Token.Kind := OBRACKETTOK;
-      ']': Token.Kind := CBRACKETTOK
+      ']': Token.Kind := CBRACKETTOK;
+      '}': Error('Closing comment } character found without opening {');
     else
-      Error('Unexpected character or end of file');
+      Error('Unexpected character "'+ch+'" ($'+Hex(ord(ch))+') found or end of file');
     end; // case
 
     ReadChar(ch);
@@ -707,5 +768,6 @@ function ScannerPos: Integer;
 begin
       Result := ScannerState.Position;
 end;
+
 
 end.
--- XDPW.pas
+++ XDPW.pas
@@ -1,9 +1,12 @@
 // XD Pascal - a 32-bit compiler for Windows
 // Copyright (c) 2009-2010, 2019-2020, Vasiliy Tereshkov
 
+// VERSION 0.14;0
+
 {$APPTYPE CONSOLE}
 {$I-}
 {$H-}
+
 
 program XDPW;
 
@@ -35,7 +38,7 @@ if DotPos > 0 then
   Name := Copy(Path, 1, DotPos - 1);
   Ext  := Copy(Path, DotPos, Length(Path) - DotPos + 1);
   end;
-  
+
 if SlashPos > 0 then
   begin
   Folder := Copy(Path, 1, SlashPos);
@@ -66,7 +69,7 @@ end;
 
 
 
-
+// Actual error message generated here
 procedure ErrorProc(ClassInstance: Pointer; const Msg: TString);
 begin
 if NumUnits >= 1 then
@@ -81,13 +84,49 @@ Halt(1);
 end;
 
 
-
+Const
+    Months: array[1..12] of string[9]=
+       ('January','February','March',   'April',   'May','     June',
+        'July',    'August', 'September','October','November', 'Decenber');
+    Days: Array[0..6] of string[9]=
+        ('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
 
 var
   CompilerPath, CompilerFolder, CompilerName, CompilerExt,
   PasPath, PasFolder, PasName, PasExt,
   ExePath: TString;
-  
+  StartTime,
+  EndTime: TSystemTime;
+  TimeString: String;
+  H,M,S,MS,
+  TotalData: Integer;
+  CompCount:Double;
+
+
+  Function Comma(K:Longint):string;
+  var
+     i:integer;
+     s: string;
+  begin
+      S := intToStr(K);
+      i := length(s)-3;
+      while i>0 do
+      begin
+          S := Copy(S,1,i) +','+copy(s,i+1,length(s));
+          I := I-3;
+      end;
+      Result := S;
+  end;
+
+function I2(N:Word):string;
+var
+   T1,t2:String[1];
+begin
+    T1 := CHR(N mod 10+ord('0'));
+    T2 := Chr(N div 10+ord('0'));
+    Result := T1+T2;
+end;
+
 
 
 begin
@@ -95,6 +134,13 @@ SetWriteProcs(nil, @NoticeProc, @WarningProc, @ErrorProc);
 
 Notice('XD Pascal for Windows ' + VERSION);
 Notice('Copyright (c) 2009-2010, 2019-2020, Vasiliy Tereshkov');
+GetLocalTime(StartTime);
+TimeString := Days[StartTime.dayOfWeek]+' '+Months[StartTime.month]+
+              ' '+IntToStr(StartTime.day)+', '+IntToStr(StartTime.year);
+TimeString := TimeString+' '+IntToStr(StartTime.Hour)+
+               ':'+I2(StartTime.Minute)+':'+I2(StartTime.Second);
+Notice('Compilation started: '+TimeString);
+
 
 if ParamCount < 1 then
   begin
@@ -115,14 +161,70 @@ InitializeCodeGen;
 Folders[1] := PasFolder;
 Folders[2] := CompilerFolder + 'units\';
 NumFolders := 2;
-   
+
+TotalLines := 0;
 CompileProgramOrUnit('system.pas');
+
 CompileProgramOrUnit(PasName + PasExt);
 
 ExePath := PasFolder + PasName + '.exe';
 LinkAndWriteProgram(ExePath);
+GetLocalTime(EndTime);
+TotalData := InitializedGlobalDataSize + UninitializedGlobalDataSize;
+Notice('Complete. Total program length was '+
+       Plural(TotalLines,'lines,','line,')+' Code size: ' + Comma(GetCodeSize) + ' ($'+
+        Hex(GetCodeSize)+') bytes. Data size: ' + Comma(TotalData )+
+        ' ($'+Hex(TotalData) + ' bytes');
+TimeString := Days[EndTime.dayOfWeek]+' '+Months[EndTime.month]+
+              ' '+IntToStr(EndTime.day)+', '+IntToStr(EndTime.year);
+TimeString := TimeString+' '+IntToStr(EndTime.Hour)+
+               ':'+I2(EndTime.Minute)+':'+I2(endTime.Second);
+Notice('Compilation completed: '+TimeString);
 
-Notice('Complete. Code size: ' + IntToStr(GetCodeSize) + ' bytes. Data size: ' + IntToStr(InitializedGlobalDataSize + UninitializedGlobalDataSize) + ' bytes');
+    H :=  EndTime.Hour;
+    if StartTime.Hour < EndTime.Hour  then
+        h:=H + 24;
+     h := h - StartTime.Hour;
+     M := EndTime.Minute ;
+     if M < StartTime.minute then
+     begin
+          H := H-1;
+          M := M+60;
+     end;
+     M := M - StartTime.minute;
+     S := EndTime.second  ;
+     if S < StartTime.second then
+     BEGIN
+        M := M-1;
+        S := S+60;
+     END;
+     S := S-StartTime.second;
+     MS := EndTime.MilliSecond;
+     IF MS < StartTime.MilliSecond then
+     begin
+        MS := MS+1000;
+        S := S-1;
+     end;
+     MS := MS-StartTime.MilliSecond;
+
+// we won't bother with days,
+// nobody is going to compile something taking that long
+   TimeString := '';
+   If H >0 then
+      Timestring := Plural(H,'hours','hour')+' ';
+   If M >0 then
+      Timestring := TimeString + Plural(M,'minutes','minute')+' ';
+   if timestring <> '' then
+      Timestring := Timestring +' and';
+   Timestring := TimeString + IntToStr(S)+'.' + IntToStr(MS)+' seconds. ';
+   TimeString := 'Compilation took '+TimeString;
+
+   S:= (H*3600 + M*60 + S);
+   compcount := ( TotalLines / (S+MS/1000))+0.5;
+   TotalLines := TRUNC(Compcount);
+
+    TimeString := TimeString+'Compilation rate was approx. '+Comma(Totallines)+' per second.';
+    Notice(TimeString);
 
 repeat FinalizeScanner until not RestoreScanner;
 FinalizeCommon;
